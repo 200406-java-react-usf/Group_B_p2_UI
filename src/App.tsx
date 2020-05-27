b54f998544dbc95243818b27b65a04b8dbadcfe9
KaneishaR <kaneisha.reese@gmail.com>
2020-05-26 19:51:14 -0500

    Added Provider and coneection to Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Provider } from 'react-redux';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { store } from './Store';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store ={store}>
+        <Router>
+          <AppBar color="primary" position="static">
+            <Toolbar>
+              <Typography variant="h5" color="inherit">
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            {/*Routes to different components goes here.
+              Be sure to import from container file */}
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
