f570c182c8accac81cfe3f295465b387a08c1346
KaneishaR <kaneisha.reese@gmail.com>
2020-05-30 18:20:26 -0500

    Conditionally render navbar components

--- src/components/navbar-component/NavbarComponent.tsx
+++ src/components/navbar-component/NavbarComponent.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { User } from '../../models/User';
-import { makeStyles, List, ListItem, Typography, ListItemText, Select, Menu, MenuItem, Button, ClickAwayListener, IconButton } from '@material-ui/core';
+import { makeStyles, List, ListItem, Typography, ListItemText, Select, Menu, MenuItem, Button, ClickAwayListener, IconButton, Badge } from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+//Navbar Properties passed from container
 interface INavbarProps {
     authUser: User;
     errorMessage: string;
+    itemCount: number;  //will update based on actions elsewhere
     logoutAction: () => void;
 }
-
+//Navbar Style Set
 const useStyles = makeStyles({
     link: {
         textDecoration: "none",
@@ -47,55 +50,105 @@ function NavbarComponent (props: INavbarProps) {
             <List component="nav">
                 <ListItem component="div">
                     <Typography color="inherit" variant="h5">
-                        <Link to="/home">MemeStore</Link>
+                        <Link to="/home" className={classes.link}>Insert Logo Here</Link>
                     </Typography>
                     {
                         !  props.authUser ?
                         <>
                             <ListItemText inset>
                                 <Typography color="inherit" variant="h6">
-                                    <Link to="/login">Login</Link>
+                                    <Link to="/login" className={classes.link}>Login</Link>
                                 </Typography>
                             </ListItemText>
                             <ListItemText inset>
                                 <Typography color="inherit" variant="h6">
-                                    <Link to="/register">Register</Link>
+                                    <Link to="/register" className={classes.link}>Register</Link>
+                                </Typography>
+                            </ListItemText>
+                            <ListItemText inset>
+                                <Typography color="inherit" variant="h6">
+                                    <Link to="/browse" className={classes.link}>Browse Memes</Link>
                                 </Typography>
                             </ListItemText>
                             
                             <ListItemText inset>
-                            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                                Admin Control Panel
-                            </Button>
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                            >
-                                        <MenuItem disabled>Admin Control Panel</MenuItem>
-                                        <MenuItem onClick={handleClose}><Link to="/addItem">Add New Inventory Item</Link></MenuItem>
-                                        <MenuItem onClick={handleClose}><Link to="/addItem">Update New Inventory Item</Link></MenuItem>
-                                        <MenuItem onClick={handleClose}><Link to="/addItem">Remove New Inventory Item</Link></MenuItem>
-                            </Menu>      
+                                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                                    Admin Control Panel
+                                </Button>
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                >
+                                            <MenuItem disabled>Admin Control Panel</MenuItem>
+                                            <MenuItem onClick={handleClose}><Link to="/items">View Inventory</Link></MenuItem>
+                                            <MenuItem onClick={handleClose}><Link to="/addItem">Add Item</Link></MenuItem>
+                                </Menu>      
                             </ListItemText >
-                            <ListItem>
-                                <Button>
+                            <ListItemText inset>
+                                <Badge color ="secondary" badgeContent={4}>
                                     <Link to="/cart" className={classes.logout} onClick={userLogout}>
                                          <ShoppingCartIcon /> 
                                     </Link>
-                                 </Button>
-                            </ListItem>
+                                 </Badge>
+                            </ListItemText>
                             <ListItemText inset>
                                 <Typography color="secondary" variant="h6">
                                     <Link to="/login" className={classes.logout} onClick={userLogout}>Logout</Link>
                                 </Typography>
                             </ListItemText>
                             
-                        </>
+                            </> 
                         :
+                        props.authUser.role_name ==='admin' ?
                         <>
+                            <ListItemText inset>
+                                <Typography color="inherit" variant="h6">
+                                    <Link to="/browse" className={classes.link}>Browse Memes</Link>
+                                </Typography>
+                            </ListItemText>
+                            <ListItemText inset>
+                                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                                    Admin Control Panel
+                                </Button>
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                >
+                                            <MenuItem disabled>Admin Control Panel</MenuItem>
+                                            <MenuItem onClick={handleClose}><Link to="/items">View Inventory</Link></MenuItem>
+                                            <MenuItem onClick={handleClose}><Link to="/addItem">Add Item</Link></MenuItem>
+                                </Menu>      
+                            </ListItemText >
 
+                            <ListItemText inset>
+                                <Typography color="secondary" variant="h6">
+                                    <Link to="/login" className={classes.logout} onClick={userLogout}>Logout</Link>
+                                </Typography>
+                            </ListItemText>
+                        </>
+                         :
+                        <>
+                            <ListItemText inset>
+                                <Typography color="inherit" variant="h6">
+                                    <Link to="/browse" className={classes.link}>Browse Memes</Link>
+                                </Typography>
+                            </ListItemText>
+                            <ListItemText inset>
+                                <Badge color ="secondary" badgeContent={props.itemCount}>
+                                    <Link to="/cart" className={classes.logout} onClick={userLogout}>
+                                         <ShoppingCartIcon /> 
+                                    </Link>
+                                 </Badge>
+                            </ListItemText>
+                            <ListItemText inset>
+                                <Typography color="secondary" variant="h6">
+                                    <Link to="/login" className={classes.logout} onClick={userLogout}>Logout</Link>
+                                </Typography>
+                            </ListItemText>
                         </>
                     }
                 </ListItem>
