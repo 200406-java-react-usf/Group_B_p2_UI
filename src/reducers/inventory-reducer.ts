d030242ee668ccf7dd21c8d4874b1e9f295518f3
wandeltk <wandeltk@gmail.com>
2020-05-30 14:08:08 -0600

    init inventory reducer

---
+++ src/reducers/inventory-reducer.ts
@@ -0,0 +1,11 @@
+import { AnyAction } from "redux";
+import { IInventoryState } from ".";
+
+const initialState: IInventoryState ={
+
+}
+
+
+export const inventoryReducer = (state: IInventoryState = initialState, action: AnyAction) => {
+
+}
