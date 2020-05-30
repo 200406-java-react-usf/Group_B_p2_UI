182661d8f3a927416b09fffe0ef58000e61c4b97
Abraham Selenke <slinky@MacBook-Pro.local>
2020-05-29 22:18:31 -0600

    Added ILoginStates

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,16 @@
 import { combineReducers } from "redux";
+import { User } from "../models/User";
+import { loginReducer } from "./login-reducer";
+
+export interface ILoginState {
+    authUser: User;
+    errorMessage: string;
+}
 
 export interface IState {
-
+    login: ILoginState;
 }
 
 export const state = combineReducers<IState>({
-
+    login: loginReducer
 });
