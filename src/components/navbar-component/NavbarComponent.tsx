20ccf5ccddff65650dff350a0e4ac479744209b9
KaneishaR <kaneisha.reese@gmail.com>
2020-06-03 06:31:56 -0500

    added conditional rendering for login

--- src/components/navbar-component/NavbarComponent.tsx
+++ src/components/navbar-component/NavbarComponent.tsx
@@ -71,7 +71,7 @@ function NavbarComponent (props: INavbarProps) {
                                 </Typography>
                             </ListItemText>
                             
-                            <ListItemText inset>
+                            {/* <ListItemText inset>
                                 <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
                                     Admin Control Panel
                                 </Button>
@@ -97,7 +97,7 @@ function NavbarComponent (props: INavbarProps) {
                                 <Typography color="secondary" variant="h6">
                                     <Link to="/login" className={classes.logout} onClick={userLogout}>Logout</Link>
                                 </Typography>
-                            </ListItemText>
+                            </ListItemText> */}
                             
                             </> 
                         :
