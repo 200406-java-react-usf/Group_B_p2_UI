571b6dd362beaea07ee951b901262d5b596593b3
wandeltk <wandeltk@gmail.com>
2020-06-04 19:37:42 -0600

    added item details action import

--- src/components/item-details-component/ItemDetailsContainer.ts
+++ src/components/item-details-component/ItemDetailsContainer.ts
@@ -1,6 +1,7 @@
 import { IState } from "../../reducers";
 import { connect } from "react-redux";
 import ItemDetailsComponent from "./ItemDetailsComponent";
+import {detailsAction} from "../../actions/item-details-actions"
 
 
 const mapStateToProps = (state: IState) => {
