930c27a551b314d190a2ff7709a68dafbd5bfaba
KaneishaR <kaneisha.reese@gmail.com>
2020-06-05 09:56:13 -0500

    create updateInventory method

--- src/remote/inventory-service.ts
+++ src/remote/inventory-service.ts
@@ -20,3 +20,8 @@ export async function deleteInventory(id: number){
 	let response = await memeStoreClient.delete(`/memestore/inventory/delete?id=${id}`);
 	return await response.data;
 }
+
+export async function updateInventory(item_name: string, details:string, cost: number, category: string, item_image:string){
+	let response = await memeStoreClient.put('/memestore/inventory', {item_name, details,cost,category,item_image});
+	return await response.data;
+}
