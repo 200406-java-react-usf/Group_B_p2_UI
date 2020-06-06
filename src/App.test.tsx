7aadcc79f1c454c2f5e7018095bd0c54a3621c1c
KaneishaR <kaneisha.reese@gmail.com>
2020-06-06 17:06:13 -0500

    test app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { mount, shallow } from 'enzyme';
+import NavbarComponent from './components/navbar-component/NavbarComponent';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('<App />', () => {
+
+  test('renders learn react link', () => {
+    const app = render(<App />);
+    // const linkElement = getByText(/learn react/i);
+    expect(app).toBeTruthy();
+  });
+
+  test('Renders NavBarComponent', () => {
+    // Testing wrapper around the rendered component
+    // The wrapper exposes accessors to the rendered component for testing
+    const wrapper = mount(<App />);
+
+    expect(wrapper.find(NavbarComponent)).toHaveLength(1);
+  })
+
+})
