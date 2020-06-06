da232e7b752320b0aa1d81cb447e392328465415
KaneishaR <kaneisha.reese@gmail.com>
2020-06-05 22:19:24 -0500

    added Router to InvoicesComponent

--- src/components/invoices-component/InvoicesComponent.tsx
+++ src/components/invoices-component/InvoicesComponent.tsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import MaterialTable from 'material-table';
 import { User } from '../../models/User';
-import { Alert } from '@material-ui/lab';
-import { makeStyles, Select, MenuItem, Grid, Typography, Button, TableContainer, TableHead, TableRow, TableCell, Table, TableBody } from '@material-ui/core';
-import { Redirect } from 'react-router';
+import { makeStyles, Select, MenuItem, Grid, Typography, Button, TableContainer, TableHead, TableRow, TableCell, Table, TableBody, ListItem, ListItemText } from '@material-ui/core';
 import { Invoice } from '../../models/Invoice';
 import { getAllInvoices } from '../../remote/invoices-service';
-import { NONAME } from 'dns';
 
 
-export interface IAdminDashProps {
+
+export interface IInvoicesState {
     authUser: User;
 }
 
@@ -23,17 +21,19 @@ const useStyles = makeStyles({
     }
 });
 
-const InvoiceComponent = (props: IAdminDashProps) => {
+const InvoicesComponent = (props: IInvoicesState) => {
 
     const classes = useStyles();
-    const [invoices, setTableData] = useState([new Invoice(0,'',0 ,new Date)]);
+    const [invoices, setTableData] = useState([new Invoice(0,'',0 ,new Date, [])]);
 
 
     let getTableData = async () => {
+        console.log("inside get table data <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
         let result = (await getAllInvoices()).filter(function(invoice: Invoice) {
             return invoice
         });
         console.log(result);
+        
         setTableData(result);
     }
     
@@ -51,15 +51,15 @@ const InvoiceComponent = (props: IAdminDashProps) => {
                 
                 columns = {[
                     { title: 'Id', field: 'invoice_id', editable: 'never'},
-                    { title: 'Username', field: 'username', editable: 'never',  cellStyle: {textAlign: 'left'} },
+                    // { title: 'Username', field: 'username', editable: 'never',  cellStyle: {textAlign: 'left'} },
                     { title: 'Total', field: 'total_cost', editable: 'never', type: 'currency', cellStyle: {textAlign: 'left'}},
                     { title: 'Date Ordered', field: 'date_ordered' , editable: 'never', type: 'date'},
                                               
                 ]}
-            data={[
-                { invoice_id: 1, username: 'Test User', total_cost: 2.45, date_ordered: 'October 8, 2019'}
-            ]}
-            //data = {invoices}
+            // data={[
+            //     { invoice_id: 1, username: 'Test User', total_cost: 2.45, date_ordered: 'October 8, 2019'}
+            // ]}
+            data = {invoices}
             title = "Inventory Items"
             detailPanel={rowData => {
                 return (
@@ -69,7 +69,7 @@ const InvoiceComponent = (props: IAdminDashProps) => {
                                <TableHead>
                                   <TableRow className={classes.detailsTable}>
                                       <TableCell className={classes.detailsTable}>Invoice No.</TableCell>
-                                      <TableCell className={classes.detailsTable}>User</TableCell>
+                                      {/* <TableCell className={classes.detailsTable}>User</TableCell> */}
                                       <TableCell className={classes.detailsTable}>Total Cost</TableCell>
                                       <TableCell className={classes.detailsTable}>Date Ordered</TableCell>
                                       <TableCell className={classes.detailsTable}>Items Purchased</TableCell>
@@ -79,15 +79,22 @@ const InvoiceComponent = (props: IAdminDashProps) => {
                              <TableBody>
                                   <TableRow>
                                       <TableCell>{rowData.invoice_id}</TableCell>
-                                      <TableCell>{rowData.username}</TableCell>
+                                      {/* <TableCell>{rowData.username}</TableCell> */}
                                       <TableCell>{rowData.total_cost}</TableCell>
                                       <TableCell>{rowData.date_ordered}</TableCell>
-
+                                      {console.log(rowData.items.map(items => items.item_name))}
+                                      <TableCell>{rowData.items.map(items => <ListItem>{items.item_name}</ListItem>)}</TableCell>
+                                      {/* <TableCell>{rowData.items.forEach(element => {
+                                          console.log(element.item_name);
+                                          return(
+                                             <Typography>{element.item_name}</Typography>
+                                          )
+                                      })};</TableCell> */}
                                   </TableRow>
                               </TableBody>
                           </Table>
                       </TableContainer>
-                        {/* <Grid container>
+                         {/* <Grid container>
                             <Grid item xs={12}>
                                 <Grid xs={6}>
                                     <Typography>Invoice No.</Typography>
@@ -108,7 +115,7 @@ const InvoiceComponent = (props: IAdminDashProps) => {
                                 <Typography>Date ordered</Typography>
                                 <Typography>{rowData.date_ordered}</Typography>
                             </Grid>
-                        </Grid> */}
+                        </Grid>  */}
                   </div>
                 )
               }}
@@ -125,4 +132,4 @@ const InvoiceComponent = (props: IAdminDashProps) => {
 );
 }
 
-export default InvoiceComponent;
+export default InvoicesComponent;
